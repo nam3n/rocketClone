baa8a1d7bc0c06aea6ea5525d54dc3f9f4dada77
vukien <vukien104@gmail.com>
2018-05-19 23:11:51 +0700

    create project

---
+++ src/GameCanvas.java
@@ -0,0 +1,59 @@
+import javax.imageio.ImageIO;
+import javax.swing.*;
+import java.awt.*;
+import java.awt.image.BufferedImage;
+import java.io.File;
+import java.io.IOException;
+
+public class GameCanvas extends JPanel {
+
+    BufferedImage starImage;
+    BufferedImage enemyImage;
+    BufferedImage playerImage;
+
+    public int positionXStar = 400;
+    public int positionYStar = 300;
+
+    public int positionXEnemy = 500;
+    public int positionYEnemy = 0;
+
+    public int positionXPlayer = 400;
+    public int positionYPlayer = 200;
+
+    public GameCanvas() {
+        this.setSize(1024, 600);
+
+
+        // load image
+        try {
+            this.starImage = ImageIO.read(new File("resources/images/star.png"));
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        try {
+            this.enemyImage = ImageIO.read(new File("resources/images/circle.png"));
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        try {
+            this.playerImage = ImageIO.read(new File("resources/images/circle.png"));
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        this.setVisible(true);
+    }
+
+    @Override
+    protected void paintComponent(Graphics g) {
+
+        g.setColor(Color.BLACK);
+        g.fillRect(0, 0, 1024, 600);
+
+        g.drawImage(this.starImage, this.positionXStar, this.positionYStar, 5, 5, null);
+        g.drawImage(this.enemyImage, this.positionXEnemy, this.positionYEnemy, 10, 10, null);
+        g.drawImage(this.playerImage, this.positionXPlayer, this.positionYPlayer, null);
+    }
+}
---
+++ src/GameWindow.java
@@ -0,0 +1,56 @@
+import javax.swing.*;
+import java.awt.event.KeyEvent;
+import java.awt.event.KeyListener;
+
+public class GameWindow extends JFrame {
+
+    GameCanvas gameCanvas;
+    long lastTime = 0;
+
+    public GameWindow() {
+        this.setSize(1024, 600); // set size window
+
+
+        this.gameCanvas = new GameCanvas();
+        this.add(this.gameCanvas);
+
+        this.addKeyListener(new KeyListener() {
+            @Override
+            public void keyTyped(KeyEvent e) {
+            }
+
+            @Override
+            public void keyPressed(KeyEvent e) {
+                if (e.getKeyCode() == KeyEvent.VK_LEFT) {
+                    gameCanvas.positionXPlayer -= 8;
+                }
+                if (e.getKeyCode() == KeyEvent.VK_RIGHT) {
+                    gameCanvas.positionXPlayer += 8;
+                }
+
+            }
+
+            @Override
+            public void keyReleased(KeyEvent e) {
+            }
+        });
+
+
+        this.setVisible(true);
+    }
+
+    public void gameLoop() {
+        while (true) {
+            long currentTime = System.nanoTime();
+            if (currentTime - this.lastTime >= 17_000_000) {
+                this.gameCanvas.positionXStar -= 3;
+                this.gameCanvas.positionYEnemy += 2;
+                this.gameCanvas.repaint();
+                this.lastTime = currentTime;
+            }
+
+
+        }
+    }
+
+}
