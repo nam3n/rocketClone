a51aee846af7af96d2882bb86805b4eb30758ba2
vukien <vukien104@gmail.com>
2018-06-07 00:01:02 +0700

    add box collider

--- src/base/GameObjectManager.java
+++ src/base/GameObjectManager.java
@@ -1,6 +1,9 @@
 package base;
 
+import game.bullet.Bullet;
+import game.enemy.Enemy;
 import game.player.Player;
+import physic.BoxCollider;
 
 import java.awt.*;
 import java.util.ArrayList;
@@ -23,19 +26,38 @@ public class GameObjectManager {
     }
 
     public void runAll() {
-        this.list.forEach(gameObject -> gameObject.run());
+        this.list
+                .stream()
+                .filter(gameObject -> gameObject.isAlive)
+                .forEach(gameObject -> gameObject.run());
         this.list.addAll(this.tempList);
         this.tempList.clear();
     }
 
     public void renderAll(Graphics graphics) {
-        this.list.forEach(gameObject -> gameObject.render(graphics));
+        this.list
+                .stream()
+                .filter(gameObject -> gameObject.isAlive)
+                .forEach(gameObject -> gameObject.render(graphics));
     }
 
     public Player findPlayer() {
         return (Player) this.list
                 .stream()
                 .filter(gameObject -> gameObject instanceof Player)
+                .findFirst()
+                .orElse(null);
+    }
+
+    public Enemy checkCollision(Bullet bullet) {
+        return (Enemy) this.list
+                .stream()
+                .filter(gameObject -> gameObject.isAlive)
+                .filter(gameObject -> gameObject instanceof Enemy)
+                .filter(gameObject -> {
+                    BoxCollider other = ((Enemy) gameObject).boxCollider;
+                    return bullet.boxCollider.checkBoxCollider(other);
+                })
                 .findFirst()
                 .orElse(null);
     }
--- src/game/enemy/EnemyShoot.java
+++ src/game/enemy/EnemyShoot.java
@@ -15,12 +15,12 @@ public class EnemyShoot {
     public void run(Enemy enemy) {
         // create bullet
         if (this.frameCounter.run()) {
-            for (double angle = 0.0; angle <= 360.0; angle += 15.0) {
-                Bullet bulletEnemy = new Bullet();
-                bulletEnemy.position.set(enemy.position);
-                bulletEnemy.velocity.set((new Vector2D(3, 0)).rotate(angle));
-                GameObjectManager.instance.add(bulletEnemy);
-            }
+//            for (double angle = 0.0; angle <= 360.0; angle += 15.0) {
+//                Bullet bulletEnemy = new Bullet();
+//                bulletEnemy.position.set(enemy.position);
+//                bulletEnemy.velocity.set((new Vector2D(3, 0)).rotate(angle));
+//                GameObjectManager.instance.add(bulletEnemy);
+//            }
             this.frameCounter.reset();
         }
 
